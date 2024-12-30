46c880ec0cea17f90e4d6bb9654dd0cee7a1c96f
Jiading Guo <zingdle@gmail.com>
2024-12-30 21:26:07 +0800

    update bench

--- test/bench.cpp
+++ test/bench.cpp
@@ -1,41 +1,47 @@
 #include <benchmark/benchmark.h>
-
-#include <random>
-
-#include <hdrcpp/hdr_histgram.hpp>
 #include <hdr/hdr_histogram.h>
 
-static void BM_hdr_cpp_record_values(benchmark::State &state) {
+#include <hdrcpp/hdr_histgram.hpp>
+#include <random>
+
+static std::vector<int64_t> generate_random_latency(int n, int64_t max_value) {
   std::random_device rd;
   std::mt19937 gen(rd());
-  // gama distribution shape 1 scale 100000
   std::gamma_distribution<double> latency_gamma_dist(1, 100000);
 
+  std::vector<int64_t> latency(n);
+  for (int i = 0; i < n; i++) {
+    int64_t number = int64_t(latency_gamma_dist(gen)) + 1;
+    number = number > max_value ? max_value : number;
+    latency[i] = number;
+  }
+
+  return latency;
+}
+
+static void BM_hdr_cpp_record_values(benchmark::State &state) {
+  const int N = 10000;
+  auto latency = generate_random_latency(N, 360000);
+
   hdrcpp::HdrHistogram<1, 360000, 2> h;
-  int64_t max_value = 360000;
   for (auto _ : state) {
-    for (int i = 0; i < 10000; i++) {
-      int64_t number = int64_t(latency_gamma_dist(gen)) + 1;
-      number = number > max_value ? max_value : number;
-      h.record_values(number);
+    for (int i = 0; i < N; i++) {
+      benchmark::DoNotOptimize(h.record_values(latency[i]));
+      benchmark::ClobberMemory();
     }
   }
 }
 
 static void BM_hdr_c_record_values(benchmark::State &state) {
-  std::random_device rd;
-  std::mt19937 gen(rd());
-  // gama distribution shape 1 scale 100000
-  std::gamma_distribution<double> latency_gamma_dist(1, 100000);
+  const int N = 10000;
+  auto latency = generate_random_latency(N, 360000);
 
   struct hdr_histogram *h;
   hdr_init(1, 360000, 2, &h);
-  int64_t max_value = 360000;
   for (auto _ : state) {
-    for (int i = 0; i < 10000; i++) {
-      int64_t number = int64_t(latency_gamma_dist(gen)) + 1;
-      number = number > max_value ? max_value : number;
-      hdr_record_value(h, number);
+    for (int i = 0; i < N; i++) {
+      benchmark::DoNotOptimize(hdr_record_value(h, latency[i]));
+      benchmark::ClobberMemory();
     }
   }
 }
